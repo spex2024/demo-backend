d98187d5f81500686f1c3a78e5cbd60e48c444ac
spex2024 <dercolbags@gmail.com>
2025-04-17 12:39:34 +0000

    v1.0.0.0

--- controller/vendor.js
+++ controller/vendor.js
@@ -620,7 +620,7 @@ export const getVendor= async (req, res) => {
         res.status(200).json(vendor);
     } catch (error) {
         console.error(error.message);
-        res.status(500).send('Server Error');
+        res.status(500).send(`${error.message}`);
     }
 };
 
